9feab792ad41bd4dbe3946e818d9cd292bec7fc5
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-04-19 22:37:51 -0500

    Edited styling for admin and navigation pages.

--- admin.php
+++ admin.php
@@ -1,19 +1,31 @@
 <?php include "partials/header.php"; ?>
 <?php
-    
+   
 
     if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false){
         header('Location: login.php');
         exit;
     }
 
-   
+    // if($_SESSION['role'] != 'admin'){
+    //     header('Location: index.php');
+    //     exit;
+    // }
+
+    
 ?>
+    <div class="container">
+        <div class="admin-navbar">
+            <div class="admin-ul">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </div>
+        </div>
+        <div class="admin-main">
+            <h1>Hello Admin <?php echo $_SESSION['username']; ?></h1>
+           
+        </div>
+    </div>
 
 
-    <h1>Hello Admin <?php echo $_SESSION['username']; ?></h1>
-    <br>
-    <a href="index.php">Home</a>
-    <br>
-    <a href="logout.php">Logout</a>
 <?php include "partials/footer.php"; ?>
