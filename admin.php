f68d34d5016b0391e3d78a90e91b930d66ec7fbf
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-04-21 22:50:59 -0500

    edited admin and finctions pages.

--- admin.php
+++ admin.php
@@ -1,16 +1,17 @@
 <?php include "partials/header.php"; ?>
+<?php include "functions.php"; ?>
 <?php
    
 
-    if(!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] == false){
+    if(!isLoggedIn()){
         header('Location: login.php');
         exit;
     }
 
-    // if($_SESSION['role'] != 'admin'){
-    //     header('Location: index.php');
-    //     exit;
-    // }
+        if($_SESSION['role'] != 'admin'){
+            header('Location: index.php');
+            exit;
+        }
 
     
 ?>
--- functions.php
+++ functions.php
@@ -2,14 +2,18 @@
 
 
 
-function setActiveClass($pageName){
-    $current_page = basename($_SERVER['PHP_SELF']);
-    return  ($current_page === $pageName) ? 'active' : '';
-}
+    function setActiveClass($pageName){
+        $current_page = basename($_SERVER['PHP_SELF']);
+        return  ($current_page === $pageName) ? 'active' : '';
+    }
 
-function isLoggedIn(){
-    return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
-}
+    function getPageclass(){
+        return basename($_SERVER['PHP_SELF'],'.php');
+    }
+
+    function isLoggedIn(){
+        return isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+    }
 
 
 ?>
