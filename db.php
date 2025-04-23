c1877efb4f475c909a22806989d579855cbd5f09
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-04-22 22:00:24 -0500

    edited style and db pages for functionality and asthetics.

--- db.php
+++ db.php
@@ -2,17 +2,17 @@
 
     $dbs = mysqli_connect("localhost", "Fatality", "Yennefer0974", "Login_App");
 
-    if($dbs){
-        echo "Connected to the database successfully";
-        echo "<br>";
-        echo "Database name: ". mysqli_get_server_info($dbs);
-        echo "<br>";
-    }else{
-        echo "Failed to connect to the database";
-        echo "<br>";
-        echo "Error: ". mysqli_connect_error();
-        echo "<br>";
-        exit();
-    }
+    // if($dbs){
+    //     echo "Connected to the database successfully";
+    //     echo "<br>";
+    //     echo "Database name: ". mysqli_get_server_info($dbs);
+    //     echo "<br>";
+    // }else{
+    //     echo "Failed to connect to the database";
+    //     echo "<br>";
+    //     echo "Error: ". mysqli_connect_error();
+    //     echo "<br>";
+    //     exit();
+    // }
 
 ?>
