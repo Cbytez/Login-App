0cb732add8c99b54132e005df8b3741b98d748a1
Christopher Bobka <ThreeDTechDev@hotmail.com>
2025-05-08 03:11:53 -0500

    Modified index page for link placement and styling.

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 
 <?php
-    // if(isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin'){
-    //     redirect('admin.php');
-    // }
+    if(isset($_SESSION['user_role']) && $_SESSION['user_role'] === 'admin'){
+        redirect('admin.php');
+    }
 
     
 ?>
@@ -15,18 +15,17 @@
           <div class="hero-image-container">
             <div class="hero-image">
                 <div class="hero-image-text">
-                    <h1>Welcome To Our PHP Login App</h1>
-                    <br>
-                        <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true): ?>
-                            <h2>Welcome <?php echo $_SESSION['username']; ?></h2>
-                            <br>
-                            <a href="create-user.php" class="index-link">Create User</a>
-                            <br>
-                            <a href="read-user.php" class="index-link">Read User</a>
-                        <?php endif; ?>
+                    <h1>Welcome To Our PHP Login App</h1>                    
                 </div>
             </div>
             <div class="hero-content">
+                <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true): ?>
+                    <h1 class="hero-content-text">Welcome <?php echo $_SESSION['username']; ?></h1>
+                    <br>
+                    <a href="create-user.php" class="index-place">Create User</a>
+                    <br>
+                    <a href="read-user.php" class="index-place">Read User</a>   
+                <?php endif; ?>
                 <p>Securely login and manage your account with us.</p>
             </div>
           </div>
